d00f122e6df0f5ce6e31b1502e13f750601ad87d
Yaroslav Nikolaev <beyn54@gmail.com>
2026-06-16 18:05:42 +0300

    booking router

--- api/api_v1/__init__.py
+++ api/api_v1/__init__.py
@@ -2,8 +2,10 @@ from fastapi import APIRouter
 
 from .auth.views import router as auth_router
 from .slots.views import router as slots_router
+from .bookings.views import router as bookings_router
 
 
 router = APIRouter()
 router.include_router(router=auth_router, prefix="/auth")
 router.include_router(router=slots_router, prefix="/slots")
+router.include_router(router=bookings_router, prefix="/bookings")
